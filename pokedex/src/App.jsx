e4722a5561040247e4534ecd9b68171eb1699869
anand-bits <h20220277@pilani.bits-pilani.ac.in>
2023-12-14 22:53:38 +0530

    Pokemen Api calling is working...

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Pokedex from './components/Pokedex/Pokedex'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>Pokedex
+    <>
+    <Pokedex/>
     </>
   )
 }
---
+++ pokedex/src/components/Search/Search.jsx
@@ -0,0 +1,21 @@
+import './Search.css';
+
+function Search()
+{
+
+    return(
+        <div className="Search-Wrapper">
+        <input
+          id='pokemon-name-search'
+          type="text"
+          placeholder="pokemon name"
+        />
+      </div>
+      
+    )
+
+
+
+}
+
+export default Search;
